54270e259d73e2687c2997feb2f89e80ae99cbc1
Michael Rayson <rays0005@algonquinlive.com>
2020-05-27 11:38:18 -0400

    Checkpoint

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear);
+    }
+
+    public void displayToastMsg(View view) {
+        Toast toast = Toast.makeText(this, getResources().getString(R.string.toast_message), Toast.LENGTH_LONG);
+        toast.show();
     }
 }
