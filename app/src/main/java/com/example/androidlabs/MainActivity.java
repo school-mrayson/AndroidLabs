5d9339d4de0da947ccb1015c386f2b80ae5a3d0f
Michael Rayson <rays0005@algonquinlive.com>
2020-05-29 11:45:13 -0400

    Linear Layout Completed

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -4,7 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +19,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_linear);
-    }
 
-    public void displayToastMsg(View view) {
-        Toast toast = Toast.makeText(this, getResources().getString(R.string.toast_message), Toast.LENGTH_LONG);
-        toast.show();
+        Button button1 = findViewById(R.id.button2);
+        CheckBox check = findViewById(R.id.checkBox2);
+        Switch switchA = findViewById(R.id.switch1);
+
+        button1.setOnClickListener(btn -> Toast.makeText(this, "Here is more information", Toast.LENGTH_LONG).show());
+        check.setOnCheckedChangeListener( (box, isChecked) -> {
+            if (box.isChecked()){
+                Snackbar.make(check, "Switch is now on", Snackbar.LENGTH_LONG).setAction("Undo", v -> check.setChecked(false)).show();
+            }else{
+                Snackbar.make(check, "Switch is now off", Snackbar.LENGTH_LONG).show();
+            }
+        });
+        switchA.setOnCheckedChangeListener( (s, isChecked) -> {
+            if (s.isChecked()){
+                Snackbar.make(switchA, "Switch is now on", Snackbar.LENGTH_LONG).setAction("Undo", v -> switchA.setChecked(false)).show();
+            }else{
+                Snackbar.make(switchA, "Switch is now off", Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 }
