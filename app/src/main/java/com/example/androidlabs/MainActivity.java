8aad79101e3c2413991ea23a26836b52768b3235
Michael Rayson <rays0005@algonquinlive.com>
2020-06-15 13:40:41 -0400

    lab3 init

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -11,32 +13,20 @@ import android.widget.Toast;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+    private SharedPreferences.Editor spEditor = sp.edit();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_relative);
+        setContentView(R.layout.activity_main_linear);
 
-        Button button1 = findViewById(R.id.button2);
-        CheckBox check = findViewById(R.id.checkBox2);
-        Switch switchA = findViewById(R.id.switch1);
 
-        button1.setOnClickListener(btn -> Toast.makeText(this, this.getString(R.string.toast_message), Toast.LENGTH_LONG).show());
+    }
 
-        check.setOnCheckedChangeListener( (box, isChecked) -> {
-            if (box.isChecked()){
-                Snackbar.make(check, this.getString(R.string.switch_on), Snackbar.LENGTH_LONG).setAction(this.getString(R.string.Undo), v -> check.setChecked(false)).show();
-            }else{
-                Snackbar.make(check, this.getString(R.string.switch_off), Snackbar.LENGTH_LONG).show();
-            }
-        });
-
-        switchA.setOnCheckedChangeListener( (s, isChecked) -> {
-            if (s.isChecked()){
-                Snackbar.make(switchA, this.getString(R.string.switch_on), Snackbar.LENGTH_LONG).setAction(this.getString(R.string.Undo), v -> switchA.setChecked(false)).show();
-            }else{
-                Snackbar.make(switchA, this.getString(R.string.switch_off), Snackbar.LENGTH_LONG).show();
-            }
-        });
+    @Override
+    protected void onPause() {
+        super.onPause();
+        spEditor.putString("userEmail", )
     }
 }
